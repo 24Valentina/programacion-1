6a8191dc0a5a95abaecafa43339c215faf75ed0b
24Valentina <karenv.valbuenar@uqvirtual.edu.co>
2023-08-23 09:18:06 -0500

    se agrego la clase libro, se agregaron los metodos getter y setter y se crearon las instancias

---
+++ Biblioteca/src/main/java/co/edu/uniquindio/biblioteca/Libro.java
@@ -0,0 +1,58 @@
+package co.edu.uniquindio.biblioteca;
+
+public class Libro {
+    private String titulo;
+    private String autor;
+    private String genero;
+    private int numPaginas;
+    private int anioPublicacion;
+    private String editorial;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(int numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+}
---
+++ Biblioteca/src/main/java/co/edu/uniquindio/biblioteca/Main.java
@@ -0,0 +1,49 @@
+package co.edu.uniquindio.biblioteca;
+
+public class Main {
+    public static void main(String[] args) {
+        Libro libro232 = new Libro();
+        libro232.setTitulo("Fisica General");
+        libro232.setAutor("S. burbano");
+        libro232.setGenero("Cientifico");
+        libro232.setEditorial(" Tebar Flores");
+        libro232.setNumPaginas(794);
+        libro232.setAnioPublicacion(2019);
+
+        Libro libro233 = new Libro();
+        libro233.setTitulo("Matematicas Discretas");
+        libro233.setAutor("Carlos Gaviria");
+        libro233.setGenero("Academico");
+        libro233.setEditorial(" U de San Buenaventura");
+        libro233.setNumPaginas(252);
+        libro233.setAnioPublicacion(2015);
+
+        Libro libro234 = new Libro();
+        libro234.setTitulo("Blancanieves");
+        libro234.setAutor("Hermanos Grimm");
+        libro234.setGenero("Cuento");
+        libro234.setEditorial("Editorial Everest");
+        libro234.setNumPaginas(32);
+        libro234.setAnioPublicacion(1812);
+
+        Libro libro235 = new Libro();
+        libro235.setTitulo("Cien Años de Soledad");
+        libro235.setAutor("Gabriel Garcia Marquez");
+        libro235.setGenero("Novela");
+        libro235.setEditorial("Sudamericana");
+        libro235.setNumPaginas(471);
+        libro235.setAnioPublicacion(1967);
+
+        Libro libro236 = new Libro();
+        libro236.setTitulo("A traves de ti");
+        libro236.setAutor("Ariana Godoy");
+        libro236.setGenero("Ficcion, adulto joven");
+        libro236.setEditorial("Montena");
+        libro236.setNumPaginas(336);
+        libro236.setAnioPublicacion(2021);
+
+
+
+
+    }
+}
